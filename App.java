3b7a2afca91f0b2133b2b41293d0610ef2de74e6
Male3451 <abhaysinghroyale.6@gmail.com>
2022-03-28 04:01:15 +0530

    Sprint 1 - UserStory1 - Task 2 :Tested

--- App.java
+++ App.java
@@ -1,11 +1,40 @@
 package LockedMe.com;
+
+
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         WelcomeScreen screen1= new WelcomeScreen();
         DisplayOptions options = new DisplayOptions();
+
+
+        //initial screen
         screen1.displayMessage();
-        options.displayAvailableOptions();
-        int op = options.receivedInput();
-        System.out.println(op);
+
+        //Greeting message
+        options.displayGreetings();
+        do {
+
+            //selecting options from interface window
+            options.displayAvailableOptions();
+
+            //choosing one option
+            int op = options.receivedInput();
+
+            //functions based on choices
+            if (op==1){
+                FilesInAscendingOrder displayAscending = new FilesInAscendingOrder();
+                displayAscending.ascendingOrder();
+
+            }else if(op==2){
+                OperationsOnFile screen2 = new OperationsOnFile();
+                screen2.functioning();
+            }else {
+                new CloseApplication().endMessage();
+                break;
+            }
+        }while (true);
+
     }
 }
--- DisplayOptions.java
+++ DisplayOptions.java
@@ -2,17 +2,24 @@ package LockedMe.com;
 
 public class DisplayOptions {
     private InputFromUser userInp = new InputFromUser();
-    void displayAvailableOptions() {
+    void displayGreetings() {
         System.out.println();
         System.out.println("                     Greetings      ");
         System.out.println("                   *************\n");
-        System.out.println("Please select one of the options given below and press Enter-");
+
+    }
+
+    void displayAvailableOptions() {
+        System.out.println("* * *");
+        System.out.println("*   Main Window   *");
+        System.out.println("              * * *");
+        System.out.println("\nPlease select one of the options given below and press Enter-");
         System.out.println();
-        System.out.println("Press 1   : Display the files names");
+        System.out.println("Press 1   : Display the names of files in directory.");
         System.out.println();
         System.out.println("Press 2   : Perform any of operations such as Add, Delete, Search, etc.");
         System.out.println();
-        System.out.println("Press 3   : Close the application");
+        System.out.println("Press 3   : Close the application.");
         System.out.println();
     }
 
@@ -20,7 +27,7 @@ public class DisplayOptions {
         int k =userInp.inputForScreen();
         while (k<1 || k>3) {
             System.out.println();
-            System.out.println("You have entered wrong Integer.\n");
+            System.out.println("->You have entered wrong Integer.\n");
             k = userInp.inputForScreen();
         }
         return k;
