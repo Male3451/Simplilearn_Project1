990e950352b76840090a1213b5839774c3b15417
Male3451 <abhaysinghroyale.6@gmail.com>
2022-03-27 23:24:30 +0530

    Sprint 1 - UserStory1 - Task3 completed

---
+++ InputFromUser.java
@@ -0,0 +1,30 @@
+package LockedMe.com;
+
+import java.util.Scanner;
+
+public class InputFromUser {
+    private Scanner input = new Scanner(System.in);
+
+    int inputForScreen(){
+        boolean notint=true ;
+        int checkint =0;
+        while(notint)
+        {
+            try
+            {
+                notint = false;
+                System.out.println("Please select one of the option mentioned above");
+                System.out.print("Option : ");
+                String s = input.next();
+                checkint = Integer.parseInt(s);
+
+            }
+            catch(Exception ex)
+            {
+                notint   = true;
+                checkint = 0;
+            }
+        }
+        return checkint;
+    }
+}
