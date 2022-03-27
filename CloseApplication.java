16a35b41c04424a92a07b70d2229448fdb20a221
Male3451 <abhaysinghroyale.6@gmail.com>
2022-03-28 04:17:24 +0530

    Sprint 3 - UserStory5 - Task 2 : Completed

---
+++ CloseApplication.java
@@ -0,0 +1,11 @@
+package LockedMe.com;
+
+public class CloseApplication {
+    public void  endMessage(){
+        System.out.println();
+        System.out.println("          *                             *");
+        System.out.println("          *          Thank You          *");
+        System.out.println("          *    Application is Closed    *");
+
+    }
+}
